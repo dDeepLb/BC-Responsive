d1309c88355b77fe3bfc80ed63c0ff107c1cf271
dDeepLb <71733861+dDeepLb@users.noreply.github.com>
2023-07-19 16:16:26 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
-	function getDefaultExportFromCjs (x) {
+// ==UserScript==
+// @name BC Responsive
+// @namespace https://www.bondageprojects.com/
+// @version 1.0
+// @description An auto response script for Bondage Club
+// @author Saki Saotome
+// @include /^https:\/\/(www\.)?bondage(?:projects\.elementfx|-europe)\.com\/R\d+\/(BondageClub|\d+)(\/)?(((index|\d+)\.html)?)?$/
+// @icon  https://dynilath.gitlab.io/SaotomeToyStore/favicon.ico
+// @grant none
+// @run-at document-end
+// ==/UserScript==
+
+(function () {
+    "use strict";
+function getDefaultExportFromCjs (x) {
 		return x && x.__esModule && Object.prototype.hasOwnProperty.call(x, 'default') ? x['default'] : x;
 	}
 
@@ -627,3 +641,4 @@
 	    window.BCResponsive_Loaded = true;
 	    console.log("".concat(ModName, " v").concat(ModVersion, " loaded."));
 	})();
+})();
