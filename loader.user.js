2c04f522cf410e8eb9c79982683fddb61ddad6d8
dDeepLb <71733861+dDeepLb@users.noreply.github.com>
2023-07-22 10:39:06 +0300

    Update loader.user.js

--- loader.user.js
+++ loader.user.js
@@ -3,14 +3,16 @@
 // @namespace https://www.bondageprojects.com/
 // @version 1.0
 // @description An auto response script for Bondage Club
-// @author Saki Saotome
+// @author dDeepLb
 // @include /^https:\/\/(www\.)?bondage(?:projects\.elementfx|-europe)\.com\/R\d+\/(BondageClub|\d+)(\/)?(((index|\d+)\.html)?)?$/
 // @icon  https://dynilath.gitlab.io/SaotomeToyStore/favicon.ico
+// @downloadURL https://github.com/dDeepLb/BC-Responsive/raw/main/loader.user.js
+// @updateURL https://github.com/dDeepLb/BC-Responsive/raw/main/loader.user.js
 // @grant none
 // @run-at document-end
 // ==/UserScript==
 
 (function() {
     'use strict';
-    fetch('https://raw.githubusercontent.com/dDeepLb/BC-Responsive/main/dev.user.js').then(r=>r.text()).then(r=>{(0, eval(r))})
+    fetch('https://raw.githubusercontent.com/dDeepLb/BC-Responsive/main/user.js').then(r=>r.text()).then(r=>{(0, eval(r))})
 })();
