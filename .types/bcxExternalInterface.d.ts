921975a151088bc939a11f8898755477c8e2ffb7
dDeepLb <ddeeplb@gmail.com>
2023-11-19 05:23:21 +0200

    Add check if BCX allows talking. Change toolpit draw func, to hook into it. Fix crash with undef `Expression`

---
+++ .types/bcxExternalInterface.d.ts
@@ -0,0 +1,132 @@
+interface BCXVersion {
+  major: number;
+  minor: number;
+  patch: number;
+  extra?: string;
+  dev?: boolean;
+}
+
+//#region Rules
+interface BCX_RuleStateAPI_Generic {
+  /** The name of the rule */
+  readonly rule: string;
+  /** Definition of the rule */
+  readonly ruleDefinition: any;
+
+  /** Current condition data of the rule */
+  readonly condition: any;
+
+  /** If the rule is in effect (active and all conditions valid) */
+  readonly inEffect: boolean;
+  /** If the rule is enforced (inEffect and enforce enabled) */
+  readonly isEnforced: boolean;
+  /** If the rule is logged (inEffect and logging enabled) */
+  readonly isLogged: boolean;
+
+  /** Rule setttings */
+  readonly customData: any;
+  /** Rule internal data */
+  readonly internalData: any;
+
+  /**
+   * Triggers and logs that Player violated this rule
+   * @param targetCharacter - If the rule is against specific target different than player (e.g. sending message/beep), this adds it to log
+   * @param dictionary - Dictionary of rule-specific text replacements in logs and notifications; see implementation of individual rules
+   */
+  trigger(targetCharacter?: number | null, dictionary?: Record<string, string>): void;
+
+  /**
+   * Triggers and logs that Player attempted to violate this rule, but the attempt was blocked (for enforced rules)
+   * @param targetCharacter - If the rule is against specific target different than player (e.g. sending message/beep), this adds it to log
+   * @param dictionary - Dictionary of rule-specific text replacements in logs and notifications; see implementation of individual rules
+   */
+  triggerAttempt(targetCharacter?: number | null, dictionary?: Record<string, string>): void;
+}
+
+interface BCX_RuleStateAPI<ID extends BCX_Rule> extends BCX_RuleStateAPI_Generic {
+  readonly rule: ID;
+  readonly ruleDefinition: RuleDisplayDefinition<ID>;
+
+  readonly condition: ConditionsConditionData<"rules"> | undefined;
+
+  readonly customData: ID extends keyof RuleCustomData ? RuleCustomData[ID] | undefined : undefined;
+  readonly internalData: ID extends keyof RuleInternalData ? RuleInternalData[ID] | undefined : undefined;
+}
+
+type BCX_Rule = string;
+type BCX_RuleStateAPI<ID extends BCX_Rule> = BCX_RuleStateAPI_Generic;
+
+//#endregion
+
+interface BCX_Events {
+  curseTrigger: {
+    /** Which action the curses did to the item */
+    action: "remove" | "add" | "swap" | "update" | "color" | "autoremove";
+    /** Name of asset group that was changed */
+    group: string;
+  };
+  /**
+   * Triggers whenever player changes subscreen in BCX.
+   * Note, that some changes might not be observable by outside mod (e.g. when user simply switches to different subscreen).
+   * This can trigger even outside of `InformationSheet` screen.
+   */
+  bcxSubscreenChange: {
+    /**
+     * Whether BCX is currently showing one of custom screens, overriding the default BC screen.
+     *
+     * At the time of emitting, this value is the same as the one returned by `bcx.inBcxSubscreen()`.
+     */
+    inBcxSubscreen: boolean;
+  };
+}
+
+interface BCX_ModAPI extends BCXEventEmitter<BCX_Events> {
+  /** Name of the mod this API was requested for */
+  readonly modName: string;
+
+  /** Returns state handler for a rule or `null` for unknown rule */
+  getRuleState<ID extends BCX_Rule>(rule: ID): BCX_RuleStateAPI<ID> | null;
+}
+
+interface BCX_ConsoleInterface {
+  /** Version of loaded BCX */
+  readonly version: string;
+
+  /** Version parsed to components */
+  readonly versionParsed: Readonly<BCXVersion>;
+
+  /**
+   * Gets BCX version of another character in room
+   * @param target - The membernumber of character to get; undefined = Player
+   */
+  getCharacterVersion(target?: number): string | null;
+
+  /** Gets if BCX runs in development mode */
+  readonly isDevel: boolean;
+
+  /**
+   * Get access to BCX Mod API.
+   * @param mod - Same identifier of your mod as used for ModSDK
+   */
+  getModApi(mod: string): BCX_ModAPI;
+
+  /** Whether BCX is currently showing one of custom screens, overriding the default BC screen. */
+  inBcxSubscreen(): boolean;
+}
+
+interface Window {
+  bcx?: BCX_ConsoleInterface;
+}
+
+type BCXEvent = Record<never, unknown>;
+type BCXAnyEvent<T extends BCXEvent> = {
+  [key in keyof T]: {
+    event: key;
+    data: T[key];
+  };
+}[keyof T];
+
+interface BCXEventEmitter<T extends BCXEvent> {
+  on<K extends keyof T>(s: K, listener: (v: T[K]) => void): () => void;
+  onAny(listener: (value: BCXAnyEvent<T>) => void): () => void;
+}
--- src/Utilities/Handlers.ts
+++ src/Utilities/Handlers.ts
@@ -3,6 +3,17 @@ import { ResponsesEntryModel } from "../Models/Responses";
 import { GlobalModule } from "../Modules/Global";
 import { isSimpleChat } from "./ChatMessages";
 import { activityMessage, leaveMessage, orgasmMessage } from "./ChatMessages";
+import { ModName } from "./Definition";
+
+const doesBcxAllowsTalking = () => {
+  const isRuleWorking = (ruleName: string) => {
+    const rule = window.bcx.getModApi(ModName).getRuleState(ruleName);
+
+    return rule.inEffect && rule.isEnforced;
+  };
+
+  if (isRuleWorking("speech_forbid_open_talking") || isRuleWorking("speech_specific_sound")) return false;
+};
 
 export const orgasmHandle = (c: Character) => {
   if (!Player.BCResponsive.GlobalModule.ResponsiveEnabled) return;
@@ -11,6 +22,7 @@ export const orgasmHandle = (c: Character) => {
   if (Player.MemberNumber !== c.MemberNumber) return;
   if (!Player.BCResponsive.ResponsesModule.extraResponses.orgasm) return;
   if (ActivityOrgasmRuined) return;
+  if (window.bcx && !doesBcxAllowsTalking()) return;
 
   orgasmMessage();
 };
@@ -22,6 +34,7 @@ export const activityHandle = (dict: ActivityInfo, entry: ResponsesEntryModel) =
   if (dict.TargetCharacter.MemberNumber !== Player.MemberNumber) return;
   if (!entry || !entry?.responses) return;
   if (!entry.selfTrigger && dict.TargetCharacter === dict.SourceCharacter) return;
+  if (window.bcx && !doesBcxAllowsTalking()) return;
 
   activityMessage(dict, entry);
 };
