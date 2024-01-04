c68ba66b29fa60493c39a58d7274218bd6edfc16
dDeepLb <ddeeplb@gmail.com>
2024-01-04 05:04:02 +0200

    Bump version Add commands to definitions

--- src/Utilities/Definition.ts
+++ src/Utilities/Definition.ts
@@ -10,10 +10,12 @@ export const CMD_BCR = cmdKeyword;
 export const CMD_TOGGLE = `${cmdKeyword} toggle`;
 export const CMD_CHANGELOG = `${cmdKeyword} changelog`;
 export const CMD_VERSION = `${cmdKeyword} version`;
+export const CMD_FIX_DATA = `${cmdKeyword} fix-data`;
+export const CMD_DEBUG_DATA = `${cmdKeyword} debug-data`;
 
 export const ModName = `Responsive`;
 export const FullModName = `Bondage Club Responsive`; //¯\_(⌣̯̀ ⌣́)_/¯
-export const ModVersion = `0.6.0`;
+export const ModVersion = `0.6.1`;
 export const ModRepository = `https://github.com/dDeepLb/BC-Responsive`;
 
 export const DebugMode = false;
