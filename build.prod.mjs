d50d3714a5bc37be6220414d872cdfe913980530
dDeepLb <71733861+dDeepLb@users.noreply.github.com>
2024-01-19 14:45:07 +0200

    Dev (#15)

--- build.prod.mjs
+++ build.prod.mjs
@@ -4,27 +4,16 @@ import time from 'esbuild-plugin-time';
 import copy from 'esbuild-copy-files-plugin';
 import fs from 'fs';
 
-const config = {
-  development: {
-    serverPath: 'http://127.0.0.1:1000/dist'
-  },
-  production: {
-    serverPath: 'https://ddeeplb.github.io/BC-Responsive'
-  }
-};
-
 (async () => {
-  const environment = process.env.NODE_ENV || 'development';
-
-  const serverPath = config[environment].serverPath;
+  const serverPath = 'https://ddeeplb.github.io/BC-Responsive';
 
   await build({
     entryPoints: ['./src/Responsive.ts'],
-    bundle: true,
-    sourcemap: true,
     outfile: './dist/main.js',
     format: 'iife',
     globalName: 'Responsive',
+    bundle: true,
+    sourcemap: true,
     loader: {
       '.css': 'text',
       '.html': 'text'
