ee4fefe336db1343b748d7de94117643a7acf7a6
dDeepLb <ddeeplb@gmail.com>
2024-06-12 22:23:37 +0300

    update build script

--- build.prod.mjs
+++ build.prod.mjs
@@ -1,9 +1,22 @@
 import { build } from 'esbuild';
 import copy from 'esbuild-copy-files-plugin';
+import progress from 'esbuild-plugin-progress';
+import time from 'esbuild-plugin-time';
 import fs from 'fs';
 
 (async () => {
-  const serverPath = 'https://ddeeplb.github.io/BC-Responsive';
+  const testPath = 'http://127.0.0.1:1000/dist';
+  const prodPath = 'https://ddeeplb.github.io/BC-Responsive';
+  /** @type {boolean | undefined} */
+  let isTestServerUp = undefined;
+
+  await new Promise((resolve) => {
+    fetch(testPath)
+      .then(() => resolve(true))
+      .catch(() => resolve(false));
+  }).then((result) => {
+    isTestServerUp = result;
+  });
 
   await build({
     entryPoints: ['./src/Responsive.ts'],
@@ -11,6 +24,7 @@ import fs from 'fs';
     format: 'iife',
     globalName: 'Responsive',
     bundle: true,
+    sourcemap: isTestServerUp,
     loader: {
       '.css': 'text',
       '.html': 'text'
@@ -22,16 +36,18 @@ import fs from 'fs';
         source: ['./src/Translations'],
         target: './dist/translations',
         copyWithFolder: false
-      })
+      }),
+      progress(),
+      time()
     ],
   }).then(() => {
     let bundleContent = fs.readFileSync('./dist/main.js', 'utf-8');
     bundleContent = bundleContent.replace(
       '(() => {',
-      `(() => {\nconst serverUrl = '${serverPath}';`
+      `(() => {\nconst serverUrl = '${isTestServerUp ? testPath : prodPath}';`
     );
     fs.writeFileSync('./dist/main.js', bundleContent);
-  }).catch((error) => {
+  }).catch(() => {
     process.exit(1);
   });
 })();
