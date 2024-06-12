4518dda07a50a340e746c803b3445fa9fe702fc6
dDeepLb <ddeeplb@gmail.com>
2024-06-12 23:23:41 +0300

    add migrator to 0.7.0

---
+++ src/Migrators/DeepLib.ts
@@ -0,0 +1,41 @@
+import { BaseMigrator, getModule } from 'bc-deeplib';
+import { GlobalModule } from '../Modules/Global';
+import { ModName } from '../Utilities/Definition';
+
+export class DeepLibMigrator extends BaseMigrator {
+  get MigrationVersion(): string {
+    return '0.7.0';
+  }
+
+  Migrate(): boolean {
+    delete Player.OnlineSettings?.['BCResponsive']?.Profiles;
+    delete Player.OnlineSettings?.['BCResponsive']?.data;
+    delete Player.OnlineSettings?.['BCResponsive']?.SavedVersion;
+
+    const data = Player[ModName];
+    const mainResponses = data.ResponsesModule.mainResponses;
+
+    mainResponses.forEach((entry) => {
+      if (entry.actName == undefined) {
+        mainResponses.splice(mainResponses.indexOf(entry));
+      }
+
+      if (typeof entry.groupName == 'string') {
+        entry.groupName = [entry.groupName];
+      }
+
+      if (entry.responses == undefined) {
+        entry.responses = [''];
+      }
+    });
+
+    const globalModuleDefaults = getModule<GlobalModule>('GlobalModule').defaultSettings;
+
+    data.GlobalModule.charTalkEnabled = (data as any).GlobalModule.CharTalkEnabled ?? globalModuleDefaults.charTalkEnabled;
+    delete (data as any).GlobalModule.CharTalkEnabled;
+    data.GlobalModule.modEnabled = (data as any).GlobalModule.ResponsiveEnabled ?? globalModuleDefaults.modEnabled;
+    delete (data as any).GlobalModule.ResponsiveEnabled;
+
+    return true;
+  }
+}
