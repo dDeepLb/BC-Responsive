c126900007a0ca4058ed492d9916c9ba588ca18a
dDeepLb <ddeeplb@gmail.com>
2024-08-07 10:15:55 +0300

    simplify

--- src/Screens/Global.ts
+++ src/Screens/Global.ts
@@ -1,5 +1,6 @@
-import { Checkbox, SettingElement } from '@Types/elements';
-import { BaseSubscreen } from 'bc-deeplib';
+import { SettingElement } from '@Types/elements';
+import { GlobalModule } from '_/Modules/Global';
+import { BaseSubscreen, getModule } from 'bc-deeplib';
 import { GlobalSettingsModel } from '../Models/Base';
 
 export class GuiGlobal extends BaseSubscreen {
@@ -16,71 +17,18 @@ export class GuiGlobal extends BaseSubscreen {
   }
 
   get pageStructure(): SettingElement[][] {
-    return [[
-      <Checkbox>{
-        id: 'modEnabled',
+    const defaults = getModule<GlobalModule>('GlobalModule').defaultSettings;
+    
+    return [Object.keys(this.settings).map((key) =>
+      ({
+        id: key,
         type: 'checkbox',
-        label: 'settings.setting.modEnabled.name',
-        description: 'settings.setting.modEnabled.desc',
-        setElementValue: () => this.settings?.modEnabled ?? true,
-        setSettingValue: (val) => (this.settings.modEnabled = val)
-      },
-      <Checkbox>{
-        id: 'responsesEnabled',
-        type: 'checkbox',
-        label: 'settings.setting.responsesEnabled.name',
-        description: 'settings.setting.responsesEnabled.desc',
-        setElementValue: () => this.settings?.responsesEnabled ?? true,
-        setSettingValue: (val) => (this.settings.responsesEnabled = val)
-      },
-      <Checkbox>{
-        id: 'charTalkEnabled',
-        type: 'checkbox',
-        label: 'settings.setting.charTalkEnabled.name',
-        description: 'settings.setting.charTalkEnabled.desc',
-        setElementValue: () => this.settings?.charTalkEnabled ?? true,
-        setSettingValue: (val) => (this.settings.charTalkEnabled = val)
-      },
-      <Checkbox>{
-        id: 'doMessageInterruption',
-        type: 'checkbox',
-        label: 'settings.setting.doMessageInterruption.name',
-        description: 'settings.setting.doMessageInterruption.desc',
-        setElementValue: () => this.settings?.doMessageInterruption ?? true,
-        setSettingValue: (val) => (this.settings.doMessageInterruption = val)
-      },
-      <Checkbox>{
-        id: 'doLeaveMessage',
-        type: 'checkbox',
-        label: 'settings.setting.doLeaveMessage.name',
-        description: 'settings.setting.doLeaveMessage.desc',
-        setElementValue: () => this.settings?.doLeaveMessage ?? true,
-        setSettingValue: (val) => (this.settings.doLeaveMessage = val)
-      },
-      /* <Checkbox>{
-        type: 'checkbox',
-        label: 'settings.setting.doAddMoansOnHighArousal.name',
-        description: 'settings.setting.doAddMoansOnHighArousal.desc',
-        setElementValue: () => this.settings?.doAddMoansOnHighArousal ?? true,
-        setSettingValue: (val) => (this.settings.doAddMoansOnHighArousal = val)
-      }, */
-      <Checkbox>{
-        id: 'doPreventMessageIfBcxBlock',
-        type: 'checkbox',
-        label: 'settings.setting.doPreventMessageIfBcxBlock.name',
-        description: 'settings.setting.doPreventMessageIfBcxBlock.desc',
-        setElementValue: () => this.settings?.doPreventMessageIfBcxBlock ?? false,
-        setSettingValue: (val) => (this.settings.doPreventMessageIfBcxBlock = val)
-      },
-      <Checkbox>{
-        id: 'doShowNewVersionMessage',
-        type: 'checkbox',
-        label: 'settings.setting.doShowNewVersionMessage.name',
-        description: 'settings.setting.doShowNewVersionMessage.desc',
-        setElementValue: () => this.settings?.doShowNewVersionMessage ?? true,
-        setSettingValue: (val) => (this.settings.doShowNewVersionMessage = val)
-      }
-    ]];
+        label: `settings.setting.${key}.name`,
+        description: `settings.setting.${key}.desc`,
+        setElementValue: () => this.settings?.[key] ?? defaults?.[key],
+        setSettingValue: (val) => (this.settings[key] = val),
+      })
+    )];
   }
 
   Load(): void {
