8af571bca4d6efbe76e720f680db365508c94c54
dDeepLb <ddeeplb@gmail.com>
2024-09-06 06:04:42 +0300

    update loader

--- loader.user.js
+++ loader.user.js
@@ -2,13 +2,15 @@
 // ==UserScript==
 // @name BC Responsive
 // @namespace https://www.bondageprojects.com/
-// @version 0.6.3
+// @version 0.7.0
 // @description An auto response script for Bondage Club
 // @author Saki Saotome, dDeepLb
 // @match https://bondageprojects.elementfx.com/*
 // @match https://www.bondageprojects.elementfx.com/*
 // @match https://bondage-europe.com/*
 // @match https://www.bondage-europe.com/*
+// @match https://bondageprojects.com/*
+// @match https://www.bondageprojects.com/*
 // @match http://localhost:*/*
 // @match http://127.0.0.1:*/*
 // @icon  https://user-images.githubusercontent.com/71733861/254577970-83fb14ab-79af-46b4-9490-ea8c85dc4097.png
@@ -19,9 +21,22 @@
 
 (function () {
   "use strict";
+  const ending = 'main.js';
+  const prodPath = 'https://ddeeplb.github.io/BC-Responsive/';
+  const devPath = `${prodPath}dev/`;
+  const localPath = 'http://localhost:1000/';
+
+  const isDev = window.location.search.includes('RSP=dev');
+  const isLocal = window.location.search.includes('RSP=local');
+  const isPublic = isDev || !isLocal;
+
+  let modLink = prodPath;
+  if (isDev) modLink = devPath;
+  else if (isLocal) modLink = localPath;
+
   const script = document.createElement("script");
   script.type = "module";
   script.setAttribute("crossorigin", "anonymous");
-  script.src = `https://ddeeplb.github.io/BC-Responsive/main.js?v=${Date.now()}`;
+  script.src = `${modLink}${ending}${isPublic ? '?' + Date.now() : ''}`;
   document.head.appendChild(script);
 })();
