b6e3b42158193dcb6068c886c238f68d369fc99f
dDeepLb <ddeeplb@gmail.com>
2024-09-15 11:05:31 +0300

    update loader

--- loader.user.js
+++ loader.user.js
@@ -8,6 +8,8 @@
 // @match https://www.bondageprojects.elementfx.com/*
 // @match https://bondage-europe.com/*
 // @match https://www.bondage-europe.com/*
+// @match https://bondageprojects.com/*
+// @match https://www.bondageprojects.com/*
 // @match http://localhost:*/*
 // @match http://127.0.0.1:*/*
 // @icon  https://user-images.githubusercontent.com/71733861/254577970-83fb14ab-79af-46b4-9490-ea8c85dc4097.png
@@ -17,10 +19,23 @@
 // ==/UserScript==
 
 (function () {
-  "use strict";
-  const script = document.createElement("script");
-  script.type = "module";
-  script.setAttribute("crossorigin", "anonymous");
-  script.src = `https://ddeeplb.github.io/BC-Responsive/main.js?v=${Date.now()}`;
+  'use strict';
+  const ending = 'main.js';
+  const prodPath = 'https://ddeeplb.github.io/BC-Responsive/';
+  const devPath = `${prodPath}dev/`;
+  const localPath = 'http://localhost:1000/';
+
+  const isDev = window.location.search.includes('RSP=dev');
+  const isLocal = window.location.search.includes('RSP=local');
+  const isPublic = isDev || !isLocal;
+
+  let modLink = prodPath;
+  if (isDev) modLink = devPath;
+  else if (isLocal) modLink = localPath;
+
+  const script = document.createElement('script');
+  script.type = 'module';
+  script.setAttribute('crossorigin', 'anonymous');
+  script.src = `${modLink}${ending}${isPublic ? '?' + Date.now() : ''}`;
   document.head.appendChild(script);
 })();
