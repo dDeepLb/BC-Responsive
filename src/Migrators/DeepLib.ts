858c80ebdbe6a4bd06d96a4370e36686950728e5
dDeepLb <ddeeplb@gmail.com>
2025-01-05 00:59:18 +0200

    implement migration to new version

--- src/Migrators/DeepLib.ts
+++ src/Migrators/DeepLib.ts
@@ -1,4 +1,7 @@
-import { BaseMigrator, getModule } from 'bc-deeplib';
+import { EntryResponseType, ResponsesEntryModel, ResponsesSettingsModel } from '_/Models/Responses';
+import { GuiResponses } from '_/Screens/Responses';
+import { BaseMigrator, dataStore, getModule } from 'bc-deeplib';
+import { Guid } from 'js-guid';
 import { GlobalModule } from '../Modules/Global';
 import { ModName } from '../Utilities/Definition';
 
@@ -11,6 +14,9 @@ export class DeepLibMigrator extends BaseMigrator {
     clearUpOldStorage();
     fixPotentialyFaultyData();
     replaceOldSettings();
+    migrateOldSettings();
+
+    dataStore();
 
     return true;
   }
@@ -25,12 +31,18 @@ function clearUpOldStorage() {
   delete Player.OnlineSettings?.['BCResponsive']?.SavedVersion;
 }
 
-function fixPotentialyFaultyData() {
-  const data = Player[ModName];
-  //@ts-expect-error: Deprecated property
-  const mainResponses = data.ResponsesModule['mainResponses'];
+type OldResponsesEntryModel = {
+  actName: string;
+  groupName: string[];
+  responses: string[];
+  selfTrigger?: boolean;
+};
 
-  mainResponses.forEach((entry: any) => {
+function fixPotentialyFaultyData() {
+  const data = Player[ModName] as any;
+  const mainResponses = data.ResponsesModule['mainResponses'] as OldResponsesEntryModel[];
+
+  mainResponses.forEach((entry) => {
     if (entry.actName == undefined) {
       mainResponses.splice(mainResponses.indexOf(entry));
     }
@@ -54,4 +66,62 @@ function replaceOldSettings() {
   delete (data as any).GlobalModule.CharTalkEnabled;
   data.GlobalModule.modEnabled = (data as any).GlobalModule?.ResponsiveEnabled ?? globalModuleDefaults.modEnabled;
   delete (data as any).GlobalModule.ResponsiveEnabled;
+}
+
+function migrateOldSettings() {
+  const data = Player[ModName];
+  
+  const newResponsesModel = [] as unknown as ResponsesSettingsModel;
+  const oldResponsesModel = (data as any).ResponsesModule['mainResponses'] as OldResponsesEntryModel[];
+
+  oldResponsesModel.forEach((entry, index) => {
+    const group = AssetGroup.find((a) => a.Name == entry.groupName[1]);
+    const activity = AssetAllActivities(Player.AssetFamily).find((act) => act.Name === entry.actName);
+    const entryName = activity && group ? GuiResponses.getActivityLabel(activity, group) : entry.actName;
+    const newEntry = {
+      name: entryName,
+      guid: Guid.newGuid().toString(),
+      isEnabled: true,
+      response: [],
+      trigger: [],
+    } as ResponsesEntryModel;
+
+    newEntry.metadata = {
+      'Group': entry.groupName,
+      'Actitity': entry.actName,
+    };
+
+    newEntry.trigger.push({
+      type: 'Action',
+      direction: entry.selfTrigger ? 'Both' : 'Incoming',
+    });
+
+    const content = entry.responses.map((res) => {
+      if (res.startsWith('**')) return res.slice(2);
+      if (res.startsWith('*')) return res.slice(1);
+      if (res.startsWith('@@')) return res.slice(2);
+      if (res.startsWith('@')) return res.slice(1);
+      return res;
+    });
+
+    content.forEach(response => {
+      const responseType: EntryResponseType = ((): EntryResponseType => {
+        if (response.startsWith('**')) return 'Emote';
+        if (response.startsWith('*')) return 'EmoteSelf';
+        if (response.startsWith('@@')) return 'Action';
+        if (response.startsWith('@')) return 'ActionSelf';
+
+        return 'Speech';
+      })();
+
+      newEntry.response.push({
+        type: responseType,
+        content: response,
+      });
+    });
+
+    newResponsesModel.push(newEntry);
+  });
+
+  data.ResponsesModule = newResponsesModel;
 }
