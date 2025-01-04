bf70d35afc7db44c1e141176797e4a34298b7502
dDeepLb <ddeeplb@gmail.com>
2025-01-04 16:42:46 +0200

    split migration code into functions as more manageable approach

--- src/Migrators/DeepLib.ts
+++ src/Migrators/DeepLib.ts
@@ -8,37 +8,50 @@ export class DeepLibMigrator extends BaseMigrator {
   }
 
   Migrate(): boolean {
-    //@ts-expect-error: Deprecated property
-    delete Player.OnlineSettings?.['BCResponsive']?.Profiles;
-    //@ts-expect-error: Deprecated property
-    delete Player.OnlineSettings?.['BCResponsive']?.data;
-    //@ts-expect-error: Deprecated property
-    delete Player.OnlineSettings?.['BCResponsive']?.SavedVersion;
-
-    const data = Player[ModName];
-    const mainResponses = data.ResponsesModule.mainResponses;
-
-    mainResponses.forEach((entry) => {
-      if (entry.actName == undefined) {
-        mainResponses.splice(mainResponses.indexOf(entry));
-      }
-
-      if (typeof entry.groupName == 'string') {
-        entry.groupName = [entry.groupName];
-      }
-
-      if (entry.responses == undefined) {
-        entry.responses = [''];
-      }
-    });
-
-    const globalModuleDefaults = getModule<GlobalModule>('GlobalModule').defaultSettings;
-
-    data.GlobalModule.charTalkEnabled = (data as any).GlobalModule?.CharTalkEnabled ?? globalModuleDefaults.charTalkEnabled;
-    delete (data as any).GlobalModule.CharTalkEnabled;
-    data.GlobalModule.modEnabled = (data as any).GlobalModule?.ResponsiveEnabled ?? globalModuleDefaults.modEnabled;
-    delete (data as any).GlobalModule.ResponsiveEnabled;
+    clearUpOldStorage();
+    fixPotentialyFaultyData();
+    replaceOldSettings();
 
     return true;
   }
+}
+
+function clearUpOldStorage() { 
+  //@ts-expect-error: Deprecated property
+  delete Player.OnlineSettings?.['BCResponsive']?.Profiles;
+  //@ts-expect-error: Deprecated property
+  delete Player.OnlineSettings?.['BCResponsive']?.data;
+  //@ts-expect-error: Deprecated property
+  delete Player.OnlineSettings?.['BCResponsive']?.SavedVersion;
+}
+
+function fixPotentialyFaultyData() {
+  const data = Player[ModName];
+  //@ts-expect-error: Deprecated property
+  const mainResponses = data.ResponsesModule['mainResponses'];
+
+  mainResponses.forEach((entry: any) => {
+    if (entry.actName == undefined) {
+      mainResponses.splice(mainResponses.indexOf(entry));
+    }
+
+    if (typeof entry.groupName == 'string') {
+      entry.groupName = [entry.groupName];
+    }
+
+    if (entry.responses == undefined) {
+      entry.responses = [''];
+    }
+  });
+}
+
+function replaceOldSettings() {
+  const data = Player[ModName];
+  
+  const globalModuleDefaults = getModule<GlobalModule>('GlobalModule').defaultSettings;
+
+  data.GlobalModule.charTalkEnabled = (data as any).GlobalModule?.CharTalkEnabled ?? globalModuleDefaults.charTalkEnabled;
+  delete (data as any).GlobalModule.CharTalkEnabled;
+  data.GlobalModule.modEnabled = (data as any).GlobalModule?.ResponsiveEnabled ?? globalModuleDefaults.modEnabled;
+  delete (data as any).GlobalModule.ResponsiveEnabled;
 }
