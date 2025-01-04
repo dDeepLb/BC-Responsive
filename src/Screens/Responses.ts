194728988117000e83565642b44231c1c97182d1
dDeepLb <ddeeplb@gmail.com>
2025-01-05 00:48:40 +0200

    remove unused stuff

--- src/Screens/Responses.ts
+++ src/Screens/Responses.ts
@@ -36,49 +36,7 @@ export class GuiResponses extends BaseSubscreen {
 
   get pageStructure(): SettingElement[][] {
     return [
-      [],
-      [
-        <Input>{
-          type: 'text',
-          id: 'extra_low',
-          label: 'responses.setting.low_response.name',
-          description: 'responses.setting.low_response.desc',
-          getElementValue: () => GuiResponses.stringListShow(this.settings?.extraResponses?.low),
-          setSettingValue: (val: string) => this.settings.extraResponses.low = GuiResponses.validateInput(val) ?? this.settings.extraResponses.low
-        },
-        <Input>{
-          type: 'text',
-          id: 'extra_light',
-          label: 'responses.setting.light_response.name',
-          description: 'responses.setting.light_response.desc',
-          getElementValue: () => GuiResponses.stringListShow(this.settings?.extraResponses?.light),
-          setSettingValue: (val: string) => this.settings.extraResponses.light = GuiResponses.validateInput(val) ?? this.settings.extraResponses.light
-        },
-        <Input>{
-          type: 'text',
-          id: 'extra_medium',
-          label: 'responses.setting.medium_response.name',
-          description: 'responses.setting.medium_response.desc',
-          getElementValue: () => GuiResponses.stringListShow(this.settings?.extraResponses?.medium),
-          setSettingValue: (val: string) => this.settings.extraResponses.medium = GuiResponses.validateInput(val) ?? this.settings.extraResponses.medium
-        },
-        <Input>{
-          type: 'text',
-          id: 'extra_hot',
-          label: 'responses.setting.hot_response.name',
-          description: 'responses.setting.hot_response.desc',
-          getElementValue: () => GuiResponses.stringListShow(this.settings?.extraResponses?.hot),
-          setSettingValue: (val: string) => this.settings.extraResponses.hot = GuiResponses.validateInput(val) ?? this.settings.extraResponses.hot
-        },
-        <Input>{
-          type: 'text',
-          id: 'extra_orgasm',
-          label: 'responses.setting.orgasm_response.name',
-          description: 'responses.setting.orgasm_response.desc',
-          getElementValue: () => GuiResponses.stringListShow(this.settings?.extraResponses?.orgasm),
-          setSettingValue: (val: string) => this.settings.extraResponses.orgasm = GuiResponses.validateInput(val) ?? this.settings.extraResponses.orgasm
-        }
-      ]
+      []
     ];
   }
 
@@ -124,88 +82,14 @@ export class GuiResponses extends BaseSubscreen {
   }
 
   run() {
-
     super.run();
-
-    /* if (PreferencePageCurrent == 1) {
-      for (const Group of AssetGroup) {
-        if (Group.IsItem() && !Group.MirrorActivitiesFrom && AssetActivitiesForGroup('Female3DCG', Group.Name).length)
-          DrawAssetGroupZone(Player, Group.Zone, 0.9, 50, 50, 1, '#808080FF', 3, this.getZoneColor(Group.Name));
-      }
-
-      if (Player.FocusGroup != null) {
-        const activity = this.activities[this.activityIndex ?? 0];
-        DrawAssetGroupZone(Player, Player.FocusGroup.Zone, 0.9, 50, 50, 1, 'cyan');
-        MainCanvas.textAlign = 'center';
-        DrawBackNextButton(
-          550,
-          550,
-          600,
-          64,
-          this.getActivityLabel(activity, Player.FocusGroup),
-          'White',
-          '',
-          () => '',
-          () => ''
-        );
-        MainCanvas.textAlign = 'left';
-        if (activity) {
-          let image = 'Assets/' + Player.AssetFamily + '/Activity/' + activity.Name + '.png';
-          if (activity.Name.indexOf('Item') > -1) {
-            image = 'Icons/Dress.png';
-          }
-          DrawImageResize(image, 1170, 28, 120, 120);
-          DrawEmptyRect(1170, 28, 120, 120, 'Black', 2);
-          this.drawActivityOptions();
-        }
-      } else {
-        DrawText(getText('responses.text.select_zone'), 550, 220, 'Black', 'White');
-      }
-    } */
-
-    if (PreferencePageCurrent == 2) elementHide({ elementId: 'mainResponses' });
   }
 
   click() {
     super.click();
-
-    if (PreferencePageCurrent == 1) {
-      for (const Group of AssetGroup) {
-        if (Group.IsItem() && !Group.MirrorActivitiesFrom && AssetActivitiesForGroup('Female3DCG', Group.Name).length) {
-          const Zone = Group.Zone.find((z) => DialogClickedInZone(Player, z, 0.9, 50, 50, 1));
-          if (Zone) {
-            if (Player.FocusGroup) this.saveResponseEntry(this.currentResponsesEntry);
-            if (Player.FocusGroup === Group) return this.deselectEntry();
-            Player.FocusGroup = Group;
-            const activities = this.activities;
-            if (this.activityIndex >= activities.length) this.activityIndex = 0;
-            this.loadResponseEntry(this.currentResponsesEntry);
-          }
-        }
-      }
-
-      if (Player.FocusGroup != null) {
-        const activities = this.activities;
-        if (MouseIn(550, 550, 600, 64)) {
-          this.saveResponseEntry(this.currentResponsesEntry);
-          if (MouseX <= 550 + 300) this.activityIndex = (activities.length + this.activityIndex - 1) % activities.length;
-          else this.activityIndex = (this.activityIndex + 1) % activities.length;
-          this.loadResponseEntry(this.currentResponsesEntry);
-        }
-      }
-
-      this.handleActivityEntryClick();
-    }
-
   }
 
   exit() {
-    this.saveResponseEntry(this.currentResponsesEntry);
-    ElementRemove('mainResponses');
-
-    CharacterAppearanceForceUpCharacter = -1;
-    CharacterLoadCanvas(Player);
-    Player.FocusGroup = null;
     super.exit();
   }
 
@@ -255,29 +139,6 @@ export class GuiResponses extends BaseSubscreen {
   deselectEntry() {
     Player.FocusGroup = null;
     elementHide({ elementId: 'mainResponses' });
-  }
-
-  loadResponseEntry(entry: ResponsesEntryModel | undefined) {
-    this.elementSetValue('mainResponses', GuiResponses.stringListShow(entry?.responses as string[]) ?? []);
-  }
-
-  saveResponseEntry(entry: ResponsesEntryModel | undefined) {
-    const responses = ElementValue('mainResponses');
-    let merge: boolean = false;
-    let unmerge: boolean = false;
-    const validResponses = GuiResponses.validateInput(responses);
-
-    if (responses != '' && validResponses) {
-      if (!entry) entry = this.createEntryIfNeeded(entry);
-      if (!this.masterSet) {
-        merge = this.mergeEntry(entry, validResponses);
-        unmerge = this.unmergeEntry(entry, validResponses);
-      }
-
-      if (this.masterSet || !(merge || unmerge)) entry.responses = validResponses ?? entry.responses;
-
-      this.settings.mainResponses.sort((a, b) => a.actName.localeCompare(b.actName));
-    }
   }
 
   clearEntry(entry: ResponsesEntryModel) {
@@ -392,95 +253,5 @@ export class GuiResponses extends BaseSubscreen {
     if (!entry) entry = this.createEntryIfNeeded(entry);
 
     entry.responses = this.copiedEntry.responses ?? [''];
-    this.loadResponseEntry(entry);
-    if (GuiResponses.activityCanBeDoneOnSelf(this.currentAct()?.Name, this.currentGroup()?.Name))
-      entry.selfTrigger = this.copiedEntry.selfTrigger;
-  }
-
-  handleActivityEntryClick() {
-    // const entry = this.currentResponsesEntry;
-    this.selfAllowed = GuiResponses.activityCanBeDoneOnSelf(this.currentAct()?.Name, this.currentGroup()?.Name);
-
-    // Clear Entry
-    /* if (!!entry && MouseIn(1310, this.getYPos(0), 64, 64)) {
-      this.clearEntry(entry);
-    } */
-
-    /* if (MouseIn(1385, this.getYPos(0), 64, 64)) {
-      this.copyEntry(entry);
-    } */
-
-    /* if (MouseIn(1455, this.getYPos(0), 64, 64)) {
-      this.pasteEntry(entry);
-    } */
-    /* 
-        // Self Allowed Checkbox
-        if (MouseIn(this.getXPos(2) + 600, this.getYPos(2) - 32, 64, 64) && this.selfAllowed) {
-          entry = this.createEntryIfNeeded(entry);
-          entry.selfTrigger = !entry.selfTrigger;
-        } */
-    /* 
-        // Master Set Checkbox
-        if (MouseIn(this.getXPos(8) + 600, this.getYPos(8) - 32, 64, 64)) {
-          this.masterSet = !this.masterSet;
-        } */
-  }
-
-  drawActivityOptions() {
-    // const activityEntry = this.currentResponsesEntry;
-
-    /* if (!!activityEntry) {
-      MainCanvas.textAlign = 'center';
-      DrawButton(1310, this.getYPos(0), 64, 64, 'X', 'White', undefined, getText('responses.text.clear_entry'));
-      MainCanvas.textAlign = 'left';
-    }
-
-    MainCanvas.textAlign = 'center';
-    DrawButton(1385, this.getYPos(0), 64, 64, '', 'White', undefined, getText('responses.text.copy_entry'));
-    DrawImageResize('Icons/Export.png', 1385, this.getYPos(0), 64, 64);
-    MainCanvas.textAlign = 'left';
-
-    MainCanvas.textAlign = 'center';
-    DrawButton(1455, this.getYPos(0), 64, 64, '', 'White', undefined, getText('responses.text.paste_entry'));
-    DrawImageResize('Icons/Import.png', 1455, this.getYPos(0), 64, 64);
-    MainCanvas.textAlign = 'left'; */
-
-    // Self Allowed Checkbox
-    // this.drawCheckbox(
-    //   'responses.setting.self_trigger.name',
-    //   'responses.setting.self_trigger.desc',
-    //   activityEntry?.selfTrigger ?? false,
-    //   2,
-    //   !this.selfAllowed
-    // );
-
-    // Master Set Checkbox
-    // this.drawCheckbox('responses.setting.master_set.name', 'responses.setting.master_set.desc', this.masterSet ?? false, 8);
-
-    this.elementPosition('mainResponses', 'responses.setting.responses.name', 'responses.setting.responses.desc', 3, false);
-  }
-
-  elementSetValue(elementId: string, value: any) {
-    const element = document.getElementById(elementId) as HTMLInputElement;
-    if (!!element && value != null) element.value = value;
-  }
-
-  elementPosition(elementId: string, label: string, description: string, order: number, disabled: boolean = false) {
-    /* var isHovering = MouseIn(this.getXPos(order), this.getYPos(order) - 32, 600, 64);
-    const isValid = !!GuiResponses.validateInput(ElementValue(elementId));
-
-    DrawTextFit(
-      isValid ? `${getText(label)}` : `${getText(label)} ✖`,
-      this.getXPos(order),
-      this.getYPos(order),
-      600,
-      isHovering ? 'Red' : 'Black',
-      'Gray'
-    ); */
-
-    // ElementPosition(elementId, this.getXPos(order) + 750 + 225, this.getYPos(order), 800, 64);
-    if (disabled) ElementSetAttribute(elementId, 'disabled', 'true');
-    if (!disabled) document.getElementById(elementId)?.removeAttribute('disabled');
-    // if (isHovering) this.tooltip(getText(description));
   }
 }
